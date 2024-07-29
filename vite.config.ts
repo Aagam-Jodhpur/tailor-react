27814abcd3ee581ee48dc2c932176df40f8daf1b
Gautam <mailforgautamvg@gmail.com>
2024-07-29 13:30:51 +0530

    Build: Inject CSS into the js bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
   build: {
     lib: {
       entry: 'src/index.ts',
